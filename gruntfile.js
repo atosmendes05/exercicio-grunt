3f406de0ff57f82adc472440ad276c36e3f78812
atos mendes <atosmendes48@gmail.com>
2024-03-02 15:59:49 -0300

    primeiro commit

---
+++ gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
+            development:{
+                files:{
+                    'dev/styles/main.css': 'source/styles/main.less'
+                }
+            },
+            production:{
+                options:{
+                    compress:true
+                },
+                files: {
+                    'dist/styles/main.min.css': 'source/styles/main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js' : 'source/scripts/main.js'
+                }
+            }
+        }
+    })
+
+
+
+
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+    grunt.loadNpmTasks('grunt-contrib-less')
+
+    grunt.registerTask('default',['grunt'])
+    grunt.registerTask('build',['less:production','less:development','uglify'])
+}
---
+++ source/scripts/main.js
@@ -0,0 +1,9 @@
+
+
+i = 0
+
+while (i < 5){
+    i = i + 1
+    console.log('oiii')
+}
+
